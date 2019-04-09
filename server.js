2494d59a9b644b27f4e8f5b0a3d2a0e9e29e55bc
TajaMaughan <42856210+TajaMaughan@users.noreply.github.com>
2019-04-08 22:10:28 -0600

    server stuff

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var express = require("express");
 var app = express();
+var path = require("path");
 //sets port
 var PORT = process.env.PORT || 8000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+app.use(express.static(path.join(__dirname, 'app/public')));
 require("./app/routing/htmlRoutes")(app);
 
 //let you know server is working and give a link to the localhost
