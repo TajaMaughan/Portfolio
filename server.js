9cf61ea78e449009c9e1c00458fdd0a91b0776c9
TajaMaughan <42856210+TajaMaughan@users.noreply.github.com>
2019-04-08 21:58:51 -0600

    working on server stuff

--- server.js
+++ server.js
@@ -6,14 +6,10 @@ var PORT = process.env.PORT || 8000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var path = require("path");
-module.exports = function (app) {
-      app.get("*", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-      });
-}
+
+require("./app/routing/htmlRoutes")(app);
 
 //let you know server is working and give a link to the localhost
 app.listen(PORT, function() {
-    console.log("App listening at http://localhost:" + PORT);
-  });
+  console.log("App listening at http://localhost:" + PORT);
+});
