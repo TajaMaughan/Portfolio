f5810d1c64f96738c383e7717ed43e600b62064b
TajaMaughan <42856210+TajaMaughan@users.noreply.github.com>
2019-04-11 18:24:07 -0600

    card layout

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,19 +1,35 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
+import { GoMarkGithub } from "react-icons/go";
+import "./style.css";
 
 function Project(props) {
   return (
-    <Col md={4}>
-      <Card>
+    <Col xs={12} md={6} lg={4}>
+      <Card className="">
         <Card.Img variant="top" src={props.img} />
-        <Card.Body>
+        <Card.ImgOverlay className="text-right">
           <Card.Title>
-            <a href={props.siteURL}>{props.name}</a>
+            <a
+              href={props.repositoryURL}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <GoMarkGithub />
+            </a>
           </Card.Title>
-          <Card.Text>{props.description}</Card.Text>
-          <Card.Link href={props.repositoryURL}>Repo</Card.Link>
-        </Card.Body>
+        </Card.ImgOverlay>
+        <Card.Title className="text-center">
+          <a
+            href={props.siteURL}
+            target="_blank"
+            rel="noopener noreferrer"
+            className=""
+          >
+            {props.name}
+          </a>
+        </Card.Title>
       </Card>
     </Col>
   );
